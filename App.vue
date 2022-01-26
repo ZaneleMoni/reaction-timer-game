a455613423ef7cb5537700fb0a755486fc8f5a4d
Ntombizanele <zanelemoni4@gmail.com>
2022-01-26 20:48:03 +0200

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,69 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <h1>Young Grasshopper Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @done="endGame"/>
+  <Results v-if="showResults" :score="score"/>
+</template>
+
+<script>
+import Block from './components/Block.vue';
+import Results from './components/Results.vue';
+export default {
+  name: 'App',
+  components: {
+    Block,
+    Results
+  },
+  data(){
+    return{
+      isPlaying: false,
+      delay:null,
+      score: null,
+      showResults: false
+    }
+  },
+  methods: {
+    start(){
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    }
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+
+button {
+background: #0faf82;
+color: white;
+border: none;
+padding: 8px 16px;
+border-radius: 4px;
+font-size: 16p;
+letter-spacing: 1px;
+cursor: pointer;
+margin: 10px;
+
+}
+
+button[disabled] {
+opacity: 0.2;
+cursor: not-allowed;
+}
+</style>
